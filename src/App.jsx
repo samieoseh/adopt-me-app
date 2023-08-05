6271201f29c7ffb3b2866c876529bbe92e911749
Samuel Oseh <samieoseh@gmail.com>
2023-08-05 12:22:27 +0100

    Implemented useQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: Infinity,
+            cacheTime: Infinity,
+        },
+    },
+});
 
 const App = () => {
     return (
         <BrowserRouter>
-            <header>
-                <Link to="/">Adopt Me!</Link>
-            </header>
-            <div>
-                <Routes>
-                    <Route path="details/:id" element={<Details />} />
-                    <Route path="/" element={<SearchParams />} />
-                </Routes>
-            </div>
+            <QueryClientProvider client={queryClient}>
+                <header>
+                    <Link to="/">Adopt Me!</Link>
+                </header>
+                <div>
+                    <Routes>
+                        <Route path="details/:id" element={<Details />} />
+                        <Route path="/" element={<SearchParams />} />
+                    </Routes>
+                </div>
+            </QueryClientProvider>
         </BrowserRouter>
     );
 };
