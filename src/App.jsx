25dbdbf5d50c6086e945f93def1c74f273f242fb
Samuel Oseh <samieoseh@gmail.com>
2023-08-05 13:55:59 +0100

    Added context

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import AdoptedPetContext from "./AdoptedPetContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -13,18 +15,21 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+    const adoptedPet = useState(null);
     return (
         <BrowserRouter>
             <QueryClientProvider client={queryClient}>
-                <header>
-                    <Link to="/">Adopt Me!</Link>
-                </header>
-                <div>
-                    <Routes>
-                        <Route path="details/:id" element={<Details />} />
-                        <Route path="/" element={<SearchParams />} />
-                    </Routes>
-                </div>
+                <AdoptedPetContext.Provider value={adoptedPet}>
+                    <header>
+                        <Link to="/">Adopt Me!</Link>
+                    </header>
+                    <div>
+                        <Routes>
+                            <Route path="details/:id" element={<Details />} />
+                            <Route path="/" element={<SearchParams />} />
+                        </Routes>
+                    </div>
+                </AdoptedPetContext.Provider>
             </QueryClientProvider>
         </BrowserRouter>
     );
